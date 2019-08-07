6df52d1321218b158bca2cb0aaf65f676df81365
Maxim <maxim.maltsev@recogizer.de>
2019-08-07 17:37:29 +0200

    small refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { AppComponent } from './app.component'
 import { GaugeChartModule } from 'angular-gauge-chart'
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [AppComponent],
